42dfd72d95e672c34a34961578747e7993d6296d
Jaydeep Suthar <technicaljade@gmail.com>
2022-04-27 07:49:25 +0530

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+// import "./App.css";
+
+function App() {
+	return (
+		<>
+			{/* Top */}
+			{/* Section - Intro :: Portfolio :: Work */}
+		</>
+	);
+}
+
+export default App;
